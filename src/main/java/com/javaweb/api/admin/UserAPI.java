b1f83d3a2b537b6e60eeae9ec4a2f2f5df3a71ea
Chu Ngọc Thắng <chuthanglsz@gmail.com>
2024-07-16 16:19:55 +0700

    first

---
+++ src/main/java/com/javaweb/api/admin/UserAPI.java
@@ -0,0 +1,14 @@
+package com.javaweb.api.admin;
+
+import com.javaweb.constant.SystemConstant;
+import com.javaweb.exception.MyException;
+import com.javaweb.model.dto.PasswordDTO;
+import com.javaweb.model.dto.UserDTO;
+import com.javaweb.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserAPI{}
